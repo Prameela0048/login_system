6d3a3ebc994d234d8dbde455aac033a571645a25
Prameela0048 <prameelapatro@gmail.com>
2023-09-02 15:50:17 +0530

    added hosting

--- NewProject/settings.py
+++ NewProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7t658fk&=-oq9zrx^^5xdg=ecp116d04zmk0db*o=idk-^(sy8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
